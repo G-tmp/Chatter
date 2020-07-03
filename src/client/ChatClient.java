c2a4988538727a3cb1fd5012b9b97b399b931846
G-tmp <karen-noreply@protonmail.com>
2020-07-03 18:13:55 +0800

    initial commit

---
+++ src/client/ChatClient.java
@@ -0,0 +1,94 @@
+package client;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+/**
+ *
+ */
+public class ChatClient {
+    private String serverAddress;
+    private int serverPort;
+    private Scanner in;
+    private PrintWriter out;
+    private JFrame frame = new JFrame("Chatter");
+    private JTextField textField = new JTextField(50);
+    private JTextArea messageArea = new JTextArea(16,50);
+
+
+
+    public ChatClient(String serverAddress,int serverPort){
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.SOUTH);
+        frame.getContentPane().add(new JScrollPane(messageArea),BorderLayout.CENTER);
+        frame.pack();
+
+        // event listener,send message client input to server
+        textField.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String input = textField.getText();
+                if (input != null  &&  !input.isBlank()){
+                    out.println(input);
+                    textField.setText("");
+                }
+            }
+        });
+    }
+
+
+    // get name from client what input to TextField
+    private String getName(){
+        return JOptionPane.showInputDialog(frame,"Choose a screen name:", "Screen name selection",JOptionPane.PLAIN_MESSAGE);
+    }
+
+
+    private void run(){
+        try {
+            Socket socket = new Socket(serverAddress,serverPort);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(),true);
+
+            // get and handle server response
+            while (in.hasNextLine()){
+                // get message from server
+                String line = in.nextLine();
+                System.out.println(line);
+
+                if (line.startsWith("SUBMIT YOUR NAME")){
+                    out.println(getName());
+                }else if (line.startsWith("NAME ACCEPTED:")){
+                    this.frame.setTitle("Chatter - " + line.substring(line.indexOf(":")+1));
+                    textField.setEditable(true);
+                }else if (line.startsWith("MESSAGE")){
+                    messageArea.append(line.substring(8)+"\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            frame.setVisible(false);
+            frame.dispose();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("localhost",44583);
+
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+
+        client.run();
+    }
+}
---
+++ src/server/ChatServer.java
@@ -0,0 +1,137 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ *  1.response "submit your name"
+ *  2.response "name accepted:"+name  &&  broadcast "has joined"+name
+ *  3.response ""
+ *  4.broadcast message client input
+ *  5.broadcast message who quit
+ */
+public class ChatServer {
+
+    //  duplicates name
+    private static Set<String> names = new HashSet<>();
+
+    // output stream collection , used for broadcast
+    private static Set<PrintWriter> writers = new HashSet<>();
+
+
+    private static class Handler implements  Runnable{
+        private String name;
+        private Socket connectionSocket;
+        private Scanner in;
+        private PrintWriter out;
+
+
+
+        public Handler(Socket connectionSocket) {
+            this.connectionSocket = connectionSocket;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                // read message from socket
+                in = new Scanner(connectionSocket.getInputStream());
+                // write message to socket
+                out = new PrintWriter(connectionSocket.getOutputStream(),true);
+
+                // require name until valid  ||  null exit
+                while (true){
+                    out.println("SUBMIT YOUR NAME");
+                    name = in.nextLine();
+
+                    if (name == null)
+                        return;
+
+                    // require lock
+                    synchronized (name.intern()){
+                        if (!name.isBlank() && !names.contains(name)){
+                            names.add(name);
+                            this.name = name;
+                            break;
+                        }
+                    }
+                }
+
+                // response this.client
+                out.println("NAME ACCEPTED: "+name);
+
+                // broadcast all clients except this.client
+                for (PrintWriter pw:writers){
+                    pw.println("MESSAGE "+name+" has joined");
+                }
+                writers.add(out);
+
+
+                // receive message from this.client
+                while (true){
+                    // message from this.client input
+                    String input = in.nextLine();
+
+                    // exit
+                    if (input.equals("/quit")){
+                        return;
+                    }
+
+                    // broadcast
+                    for (PrintWriter pw:writers){
+                        pw.println("MESSAGE ["+name+"] "+input);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (out!=null){
+                    writers.remove(out);
+                }
+
+                if (name != null){
+                    System.out.println("MESSAGE "+name+" will leave");
+                    names.remove(name);
+
+                    for (PrintWriter pw:writers){
+                        pw.println("MESSAGE "+name+" has left");
+                    }
+                }
+
+                try {
+                    connectionSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(100);
+        int serverPort = 44583;
+        System.out.println("chat server is running...");
+
+        try (ServerSocket socket = new ServerSocket(serverPort)){
+            while (true)
+                pool.execute(new Handler(socket.accept()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
